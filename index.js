3901e94c0dd8a7b27ce85641991b9a719663b161
Abi506 <abinandhan506@gmail.com>
2024-02-21 23:33:40 +0530

    backend crd operations

--- index.js
+++ index.js
@@ -26,3 +26,40 @@ const databaseAndServerInitialization = async () => {
     }
   };
   databaseAndServerInitialization();
+
+  app.post("/upload-movies/",async(request,response)=>{
+    const {id,url,movieName,cast}=request.body;
+    const uploadMoviesQuery=`
+    INSERT INTO movies(id,url,movieName,cast)
+    VALUES(
+      '${id}',
+      '${url}',
+      '${movieName}',
+      '${cast}'
+    )
+    `
+    const uploadMovies=await data.run(uploadMoviesQuery);
+    console.log(uploadMovies);
+    response.send("Movie uploaded successfully")
+  })
+
+  app.get("/movies/",async(request,response)=>{
+    const moviesQuery=`
+    SELECT * FROM movies`;
+
+    const movies=await data.all(moviesQuery);
+    console.log(movies);
+    response.send(movies);
+  })
+
+  app.delete("/movies/", async (request, response) => {
+    const { id } = request.body;
+    const deleteMovieQuery = `
+      DELETE FROM movies 
+      where id = '${id}';
+    `;
+  
+    const deleteArray = await data.run(deleteMovieQuery);
+    console.log(deleteArray);
+    response.send("Movie deleted successfully");
+  });
